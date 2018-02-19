cce47ec06293a69dcc3143bbaee9bdf97c4d1826
Ohjin <koj714@daum.net>
2018-02-19 22:32:12 +0900

    ADD - JsonTest Scene

---
+++ Assets/_Scripts/Debug_PrintTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Debug_PrintTransform : MonoBehaviour {
+
+    public Text DebugText;
+
+    private void Start()
+    {
+        if (DebugText == null)
+            Debug.LogError("DebugText(Text) is null !!");
+    }
+
+    void Update () {
+        string str = "Position : " + transform.position.ToString();
+        str += "\nRotation : " + transform.rotation.eulerAngles.ToString();
+        DebugText.text = str;
+	}
+}
---
+++ Assets/_Scripts/SerializedObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SerializedObject : MonoBehaviour {
+
+    public Vector3 mPosition;
+    public Quaternion mRotation;
+    public Vector3 mScale;
+    // ... and some
+
+    private void Start()
+    {
+        SetCurrentState();
+    }
+
+    public void SetCurrentState()
+    {
+        mPosition = transform.position;
+        mRotation = transform.rotation;
+        mScale = transform.localScale;
+}
+}
