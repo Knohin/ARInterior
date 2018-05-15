a1df0a33d7ebde79b782b55e7bff37cf33da1592
blackhands77 <33307479+blackhands77@users.noreply.github.com>
2018-05-15 14:04:43 +0900

    complete UI
    start to load & select

--- Assets/_Scripts/LoadingScene.cs
+++ Assets/_Scripts/LoadingScene.cs
@@ -6,50 +6,30 @@ using UnityEngine.UI;
 
 public class LoadingScene : MonoBehaviour {
 
-    [SerializeField]
-    Image progressBar;
-	void Start () {
-        StartCoroutine(LoadScene());
-    }
-	
-	// Update is called once per frame
-	void Update () {
+    public GameObject Loading;
+    public Slider slider;
+    public Text text;
+
+    public void LoadLevel (int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
         
     }
 
-    public IEnumerator LoadScene()
+    IEnumerator LoadAsynchronously (int sceneIndex)
     {
-        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-        AsyncOperation oper = SceneManager.LoadSceneAsync("SelectUI");
-        //LoadSceneAsync ("게임씬이름"); 입니다.
-        oper.allowSceneActivation = false;
-        //allowSceneActivation 이 true가 되는 순간이 바로 다음 씬으로 넘어가는 시점입니다.
+        Loading.SetActive(true);
 
-        float timer = 0.0f;
-        while (!oper.isDone)
+        while (!operation.isDone)
         {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            text.text = progress * 100f + "%";
+
             yield return null;
-
-            timer += Time.deltaTime;
-
-            if (oper.progress >= 0.9f)
-            {
-                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, 1f, timer);
-                //Image가 아니라 Slider의 경우 progressBar.value로 간단히 구현이 가능합니다만
-                // 이미지가 찌그러진 것이 펴지는 것처럼 나오기 때문에 비추천하는 방법입니다.
-
-                if (progressBar.fillAmount == 1.0f)
-                    oper.allowSceneActivation = true;
-            }
-            else
-            {
-                progressBar.fillAmount = Mathf.Lerp(progressBar.fillAmount, oper.progress, timer);
-                if (progressBar.fillAmount >= oper.progress)
-                {
-                    timer = 0f;
-                }
-            }
         }
     }
 }
